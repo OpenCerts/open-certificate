944f3fa6cfb2f07fead3577fe0ede8016c8cfd58
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-02-07 11:42:43 +0800

    Added basic template for new certificate schema

--- utils/utils.js
+++ utils/utils.js
@@ -33,11 +33,13 @@ function bufSortJoin(...args) {
   return Buffer.concat([...args].sort(Buffer.compare))
 }
 
+// If element is not a buffer, stringify it and then hash it to be a buffer
 function toBuffer(element){
   return (Buffer.isBuffer(element) && element.length === 32)
   ? element : sha3(JSON.stringify(element));
 }
 
+// If hash is not a buffer, convert it to buffer (without hashing it)
 function hashToBuffer(hash){
   return (Buffer.isBuffer(hash) && hash.length === 32)
   ? hash: Buffer(hash, 'hex');
